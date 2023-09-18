b74646c6ef3ddfa14d62c92fbf415ada56fca801
Md Sohag <sohag.ali@valmetpartners.com>
2023-09-18 09:53:39 +0600

    updated

--- JavaScriptBegToAdv/app.js
+++ JavaScriptBegToAdv/app.js
@@ -4,6 +4,14 @@ const name2 = "Dev";
 let letVariable = "I'm a let!";
 console.log('Hey Dev Ed');
 
+// let text1 = "A";
+// let text2 = "B";
+// let result = text1 < text2;
+let text1 = "20";
+let text2 = "5";
+let result = text1 < text2;
+console.log(result);
+
 //function
 function example() {
     const name2 = "Dev";
@@ -27,4 +35,48 @@ console.log(addNumber(100,"20"));
 const myAge =20;
 console.log("My age is :"+ myAge)
 
+console.log(`My age :${myAge}`);
+
 console.log('hello it\'s me');
+
+const a = 100;
+const naam = "Edin";
+const combined = a+naam;
+console.log(typeof combined);
+
+// Operator
+// Exponentiation Assignment Example
+var x = 10;
+x **= 5;
+console.log(x);
+
+//Left Shift Assignment Example
+var x = 100;
+x <<= 5;
+console.log(x);
+
+//If/else condition
+let number = 10;
+
+// Check if the number is positive, negative, or zero
+if (number > 0) {
+  console.log("The number is positive.");
+} else if (number < 0) {
+  console.log("The number is negative.");
+} else {
+  console.log("The number is zero.");
+}
+
+//Object,Properties,Methods
+const car = {type:"Fiat", model:"500", color:"white"};
+
+const person = {
+    firstName: "John",
+    lastName : "Doe",
+    id       : 5566,
+    fullName : function() {
+      return this.firstName + " " + this.lastName;
+    }
+  };
+
+ console.log(person.fullName());
