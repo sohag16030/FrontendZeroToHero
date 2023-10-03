9ac089fee90158185ecf4af90914f0d669f7673a
Md Sohag <sohag.ali@valmetpartners.com>
2023-10-03 18:53:50 +0600

    updated

--- 5_Projects_Using_HTML_CSS_JS/test.js
+++ 5_Projects_Using_HTML_CSS_JS/test.js
@@ -1,3 +1,21 @@
-"use strict";    
-x=10;    
-console.log(x); 
+// "use strict";    
+// x=10;    
+// console.log(x); 
+
+// function display() {
+//     var arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//     arr1.copyWithin(2, 3);
+//     document.write(arr1);
+// }
+// display();
+
+// undeclaredVar = 42;
+
+// // JavaScript may treat it as a global variable
+// console.log(undeclaredVar); // Outputs: 42
+
+// i = 10;
+// i = "string;"
+// console.log(i);
+
+console.log(parseInt ("4F", 16));
