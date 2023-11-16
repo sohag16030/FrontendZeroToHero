1ba7e38a7ad8fdca1bdde8c9160291467c30e99e
Md Sohag <sohag.ali@valmetpartners.com>
2023-11-16 17:25:58 +0600

    updated

--- PIN_MATCHER/js/main.js
+++ PIN_MATCHER/js/main.js
@@ -10,7 +10,3 @@ function deleteLast() {
     var display = document.getElementById('display');
     display.textContent = display.textContent.slice(0, -1);
 }
-
-function appendRandomNumberToDisplay(){
-    var randomNumber = 
-}
